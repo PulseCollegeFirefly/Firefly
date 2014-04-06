57e58b90b2c3d50e9d575b9d73bc49cf20422578
Patchthesock <stephencallaghan91@gmail.com>
2014-04-06 18:03:24 +0100

    Updated Lerp to give it a fade feature.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,19 +7,36 @@ public class PlayerHealth : MonoBehaviour {
 	public float timer; // The amount of time taken
 	public int healthTakeOffset = 5; // The amount of time between health time damage hits.
 
+	public float fadeSpeed = 2f;
+	public float highIntensity = 1f;
+	public float lowIntensity = 0.05f;
+	public float targetIntensity;
+	public float changeMargin = 0.01f;
+	public float healthBarOn;
+
+
 	private float playerHealth;
+	private Color guiColor;
+
+
+
+	// Temparary Variables
 	private float timeDamage;
 	private float timeTemp; // Temparary time variable
 
 	private float screenWidth;
 	private float screenHeight;
 
-	void Start () {
+	void Start() {
 		// On Start set player health to 100
 		playerHealth = 100;
 
 		// Calculate Damage
 		timeDamage = playerHealth / (300 / healthTakeOffset);
+
+		// Set guiColor
+		guiColor = Color.white;
+		guiColor.a = 0;
 
 		// Cache screen width and height. 
 		screenWidth = Screen.width;
@@ -31,8 +48,9 @@ public class PlayerHealth : MonoBehaviour {
 	}
 
 	// Health Display // Needs to be overhalled with the art department. JOSH
-	void OnGUI ()
+	void OnGUI()
 	{
+		GUI.color = guiColor;
 		if(!healthBarTexture)
 		{
 			Debug.LogError("Assign a Texture in the inspector.");
@@ -40,15 +58,26 @@ public class PlayerHealth : MonoBehaviour {
 		}
 
 
-		GUI.DrawTexture(new Rect(0, 0, screenWidth, screenHeight), healthBarTexture, ScaleMode.StretchToFill, true);
+		HealthBarTextureDraw();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 
 		timer += Time.deltaTime;
 		timeTemp += Time.deltaTime;
 
+		// Change intensity of Lerp
+		if(playerHealth < 90)
+			guiColor.a = Mathf.Lerp(guiColor.a, targetIntensity, fadeSpeed * Time.deltaTime);
+
+		// Check Target Intensity
+		CheckTargetIntensity();
+
+		// Check Target Speed
+		CheckTargetSpeed();
+
+		// If player died
 		if(playerHealth <= 0)
 		{
 			dead();
@@ -62,12 +91,38 @@ public class PlayerHealth : MonoBehaviour {
 
 	}
 
-	void DamageFromTime () {
+	void DamageFromTime() {
 		playerHealth -= timeDamage;
 		Debug.Log(timeDamage + " Damage has been done.");
 	}
 
-	void dead() {
+	void dead () {
 		Debug.Log ("Player has died.");
+	}
+
+	void HealthBarTextureDraw(){
+		GUI.DrawTexture(new Rect(0, 0, screenWidth, screenHeight), healthBarTexture, ScaleMode.StretchToFill, true);
+	}
+
+	void CheckTargetIntensity()
+	{
+		if(Mathf.Abs (targetIntensity - guiColor.a) < changeMargin)
+		{
+			highIntensity = 1 - (playerHealth / 100);
+
+			if(targetIntensity == highIntensity)
+			{
+				targetIntensity = lowIntensity;
+			}
+			else
+			{
+				targetIntensity = highIntensity;
+			}
+		}
+	}
+
+	void CheckTargetSpeed()
+	{
+		return;
 	}
 }
