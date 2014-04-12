0d1937f55367ee4b5a9a950713c96104f9198504
Patchthesock <stephencallaghan91@gmail.com>
2014-04-12 21:56:42 +0100

    Finished Guard Desk

---
+++ Assets/Scripts/EnvironmentScripts/FirstFireTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstFireTrigger : MonoBehaviour {
+
+	public float radius = 5.0F;
+	public float power = 1000.0F;
+	public Transform relPos;
+
+	void OnTriggerEnter ()
+	{
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			if (hit && hit.rigidbody)
+			{
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, 3.0F);
+				Debug.Log ("Force Added");
+			}	
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerFeelForce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeelForce : MonoBehaviour {
+
+	private float externalForceAmount;
+
+	void Awake ()
+	{
+//		forceAmount = 0;
+	}
+	
+	// Update is called once per frame
+	//void FixedUpdate () {
+	//	rigidbody.AddExplosionForce
+	//}
+
+	//public addForce (float amount, string where)
+	//{
+
+	//}
+}
