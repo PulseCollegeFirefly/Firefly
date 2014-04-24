7b2524c77769a0ca34adbacd9238fd55cb1f88be
Conor Brien <conorbrien@gmail.com>
2014-04-24 11:29:20 +0100

    Added Trigger For First Fire
    Set it for the Trigger to initiate the explosion and the stumble.
    
    Big Badda Boom!

--- Assets/Scripts/EnvironmentScripts/FirstFireTrigger.cs
+++ Assets/Scripts/EnvironmentScripts/FirstFireTrigger.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class FirstFireTrigger : MonoBehaviour {
 
+	//Declares GameObject, I dragged the Explosion1 GameObject onto this instead of declaring it
+
+	public GameObject explode;
+
+	void Start()
+	{
+		//Ensures Explosion is set of off on launch
+		explode.SetActive (false);
+	}
+
 	void OnTriggerEnter (Collider other)
 	{
 		if(other.tag == "Player")
 		{
+			//Causes stumble
+
 			other.GetComponent<PlayerFeelForce>().shakeHead();
+
+			//Turns on Explosion Particle Effect and then Destroys the Trigger so it can't be re-used
+			explode.SetActive (true);
+			Destroy(this.gameObject);
 		}
 	}
 }
