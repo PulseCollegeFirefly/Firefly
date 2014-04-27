66b3591c5c6da669f149bb47b689ed2fc72de592
Patchthesock <stephencallaghan91@gmail.com>
2014-04-27 14:12:49 +0100

    Added Player Torch, only works when a Pickup Object added to the inventory that's named Torch is picked up.

---
+++ Assets/Scripts/EnvironmentScripts/InventoryObjectScripts/PlayerTorch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTorch : MonoBehaviour {
+
+	private Light torch;
+	private GameObject player;
+
+	void Awake () {
+		// Reference Torch and turn it off
+		torch = this.gameObject.GetComponent<Light>();
+		torch.enabled = false;
+
+		// Reference the player
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonUp("Torch"))
+		{
+			if(player.GetComponentInChildren<PlayerInventory>().findItem("Torch"))
+			{
+				torch.enabled = !torch.enabled;
+			}
+		}
+	}
+}
