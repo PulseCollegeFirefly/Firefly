959a8779dc46ceb36f5b0cdfcdb7ffc7a32d206e
Conor Brien <conorbrien@gmail.com>
2014-04-28 15:09:10 +0100

    After 3 seconds the door will go boom :)
    (Added Script CellDoorOpen, applied Rigidbody to Door)

---
+++ Assets/Scripts/IntroScripts/CellOpening.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellOpening : MonoBehaviour 
+{
+	public float upForce;
+	public float sideForce;
+
+	// Use this for initialization
+
+	void Start () 
+	{
+		StartCoroutine(DoorCoroutine ());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}	
+
+	public IEnumerator DoorCoroutine()		
+	{	
+		yield return new WaitForSeconds(3.0f);
+		rigidbody.AddForce(transform.forward * 1400);
+		rigidbody.AddForce(transform.up * 1400);
+		rigidbody.useGravity = true;
+		Debug.Log ("Wait Confirmed");
+	}
+}
