52ab182f56d526a6154959a4d4f8bd706a0ecdd8
Conor Brien <conorbrien@gmail.com>
2014-04-28 17:52:59 +0100

    Testing Requested!
    *You cannot move initially, but you can look around.
    *The Door will explode inwards after 3 seconds
    *after 2 more seconds you can move again
    
    (needed this to avoid player collisions with the door)

--- Assets/Scripts/IntroScripts/CellOpening.cs
+++ Assets/Scripts/IntroScripts/CellOpening.cs
@@ -3,31 +3,46 @@ using System.Collections;
 
 public class CellOpening : MonoBehaviour 
 {
+	//Force Upwards of the Explosion attached to the Celldoor
 	public float upForce;
+	//Force Outwards of the Explosion attached to the celldoor
 	public float sideForce;
+	//The Explosion Particle Effect prefab, had to be dragged manually into the inspector
 	public GameObject explode;
+	//A GameObject for the Player, can be Public and dragged or declared as down below
+	GameObject thePlayer;
 
 	// Use this for initialization
 
 	void Start () 
 	{
+		//Sets the Player FPC to the GameObject
+		thePlayer = GameObject.Find ("Player");
+		//Ensures Explosions begins as "off"
 		explode.SetActive (false);
+		//Call the Coroutine, required due to timings involved
 		StartCoroutine(DoorCoroutine ());
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
 
-	}	
 
 	public IEnumerator DoorCoroutine()		
 	{	
+		//Turns off the FirstPersonCharacter Script, prevents movement using the keys, but the mouse still looks around
+		thePlayer.GetComponent<FirstPersonCharacter> ().enabled = false;
+		//Wait for two seconds
 		yield return new WaitForSeconds(2.0f);
+		//Turn on the Explosion particle effect
 		explode.SetActive (true);
+		//Wait another second
 		yield return new WaitForSeconds(1.0f);
+
+		//Explode the door, turn on the gravity (the door is a little above the floor).
 		rigidbody.AddForce(transform.forward * sideForce);
 		rigidbody.AddForce(transform.up * upForce);
 		rigidbody.useGravity = true;
+		//Wait another two seconds
+		yield return new WaitForSeconds (2.0f);
+		//Turn back on movement
+		thePlayer.GetComponent<FirstPersonCharacter> ().enabled = true;
 	}
 }
