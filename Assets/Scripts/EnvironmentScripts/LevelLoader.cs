cee7f19dbd45567b63b9447fc32c2d28155ba5ba
Patchthesock <stephencallaghan91@gmail.com>
2014-05-02 19:30:35 +0100

    Finished With Level 1 for the night.

---
+++ Assets/Scripts/EnvironmentScripts/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public string levelToLoad;
+
+	void OnTriggerEnter (Collider other) {
+		if(other.tag == "Player")
+			Application.LoadLevel("Level02");
+	}	
+}
