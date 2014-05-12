66712e2552b457f1e559b150da500b69f6224394
Conor Brien <conorbrien@gmail.com>
2014-05-12 13:09:52 +0100

    Added Audio to Keydrop

--- Assets/Scripts/EnvironmentScripts/TriggerHandling/DyingSoldier.cs
+++ Assets/Scripts/EnvironmentScripts/TriggerHandling/DyingSoldier.cs
@@ -7,6 +7,7 @@ public class DyingSoldier : MonoBehaviour {
 	public float ySpeed;
 	public float zSpeed;
 	bool isDead = false;
+	public GameObject drop;
 
 	// Use this for initialization
 	void Start () 
@@ -20,13 +21,21 @@ public class DyingSoldier : MonoBehaviour {
 
 		if (transform.position.z < -0.3 && isDead == false)
 		{
-			animation.Play ("soldierDieBack", PlayMode.StopAll);
-			isDead = true;
+			StartCoroutine(Spawn ());
 		}
 		else
 		if(isDead == false)
 		{
 			this.transform.Translate(xSpeed *Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
 		}
+	}
+	public IEnumerator Spawn()		
+	{
+		animation.Play ("soldierDieBack", PlayMode.StopAll);
+		isDead = true;
+		drop.SetActive (true);
+		yield return new WaitForSeconds(2.0f);
+		drop.GetComponent<Rigidbody>().detectCollisions = false;
+		drop.GetComponent<Rigidbody>().useGravity = false;
 	}
 }
