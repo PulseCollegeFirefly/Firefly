bef14d7d8a338e4b2e04d479469c853d4aa6629e
Patchthesock <stephencallaghan91@gmail.com>
2014-05-16 20:01:20 +0100

    Warden's Office Updates

---
+++ Assets/Scripts/EnvironmentScripts/WardensOffice/WardensOfficeEntry.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WardensOfficeEntry : MonoBehaviour {
+
+	public GameObject[] rubbles;
+
+	// Use this for initialization
+	void Awake () {
+
+		foreach(GameObject rubble in rubbles)
+		{
+			rubble.rigidbody.useGravity = false;
+			rubble.rigidbody.isKinematic = true;
+			rubble.collider.enabled = false;
+
+			// Render
+			Switch(rubble);
+		}
+
+	}
+	
+	void OnTriggerExit (Collider p) {
+
+		if(p.tag == "Player")
+		{
+			foreach(GameObject rubble in rubbles)
+			{
+				rubble.collider.enabled = true;
+				rubble.rigidbody.useGravity = true;
+				rubble.rigidbody.isKinematic = false;
+
+				Switch (rubble);
+			}
+		}
+
+		this.gameObject.collider.isTrigger = false;
+	}
+
+	private void Switch(GameObject a)
+	{
+		Renderer[] renderers = a.GetComponentsInChildren<Renderer>();
+		
+		foreach (Renderer r in renderers)
+		{
+			r.enabled = !r.enabled;
+		}
+	}
+}
