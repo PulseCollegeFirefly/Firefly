82c5cd82504b7cbb25f9dddeaec85175b5b41203
BezleySnipes <David.Mulhall@gmail.com>
2014-05-16 20:21:38 +0100

    Sirens and alarm audio

---
+++ Assets/Scripts/EnvironmentScripts/SirenRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SirenRotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(500, 0, 0) * Time.deltaTime);
+	}
+}
