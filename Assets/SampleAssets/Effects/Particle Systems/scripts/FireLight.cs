66ae34f80f16957099220f7b9259266f4af83ce3
Patchthesock <stephencallaghan91@gmail.com>
2014-05-17 15:51:32 +0100

    Fire Shadow Changes

--- Assets/SampleAssets/Effects/Particle Systems/scripts/FireLight.cs
+++ Assets/SampleAssets/Effects/Particle Systems/scripts/FireLight.cs
@@ -5,10 +5,11 @@ public class FireLight : MonoBehaviour {
 
 	float rnd;
 	bool burning = true;
+	public float variablize =.1f;
 
 	void Start()
 	{
-		rnd = Random.value * 100;
+		rnd = Random.value * 50f;
 		//audio.time = Random.Range (0f, audio.clip.length);
 	}
 
@@ -20,7 +21,7 @@ public class FireLight : MonoBehaviour {
 			float x = Mathf.PerlinNoise(rnd+0+Time.time*2,rnd+1+Time.time*2)-0.5f;
 			float y = Mathf.PerlinNoise(rnd+2+Time.time*2,rnd+3+Time.time*2)-0.5f;
 			float z = Mathf.PerlinNoise(rnd+4+Time.time*2,rnd+5+Time.time*2)-0.5f;
-			transform.localPosition = Vector3.up + new Vector3(x,y,z)*1;
+			transform.localPosition = Vector3.up + new Vector3(x,y,z)*variablize;
         }
 	}
 
