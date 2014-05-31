a99a4e1cc0e9ca63b495f240160fbcdeeedd0806
Patchthesock <stephencallaghan91@gmail.com>
2014-05-31 02:47:18 +0100

    Added Placeable one time audio trigger to prefabs. Sounds need to be 2D. tried calling the player audio source but footsteps keep over writing not worth the hassle, just make 2D sounds :D

---
+++ Assets/Scripts/EnvironmentScripts/TriggerHandling/AudioTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrigger : MonoBehaviour {
+
+	public AudioClip audioClip;
+
+	private bool hasPlayed = false;
+
+	void OnTriggerEnter (Collider p)
+	{
+		if(p.tag == "Player")
+		{
+			if(!hasPlayed)
+			{
+				this.gameObject.audio.PlayOneShot (audioClip);
+				hasPlayed = true;
+			}
+
+		}
+	}
+}
