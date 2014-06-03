59a8d6b13b7a7087c65c63e6ebc8a29ffdfa1858
bezleysnipes <David.mulhall@gmail.com>
2014-06-03 20:13:55 +0100

    Forced model reset - sorry if i broke it

---
+++ Assets/CamControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public Camera mainCamera;
+	public Camera teapotCamera;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
